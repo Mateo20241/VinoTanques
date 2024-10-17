eeb8d52d89b9c06cfd7c61098e943c47b9f4739b
Mateo <jumninom@udistrital.edu.co>
2024-10-17 18:58:18 -0500

    Se agrega el ejemplo 2 del proyecto

---
+++ Programa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el nombre del vino:");
+        string nombreVino = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el radio del tanque en metros:");
+        double radio = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del tanque en metros:");
+        double altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el tipo de material (madera o metal):");
+        string tipoMaterial = Console.ReadLine();
+
+        // Crear un objeto de la clase Tanque
+        Tanque tanque = new Tanque(radio, altura, tipoMaterial);
+
+        // Calcular el volumen y convertir a litros (1 m³ = 1000 litros)
+        double volumenLitros = tanque.CalcularVolumen() * 1000;
+
+        // Mostrar el mensaje final
+        Console.WriteLine($"Hay {volumenLitros:F2} litros de {nombreVino} en un tanque de {tipoMaterial}.");
+    }
+}
---
+++ Tanque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tanque
+{
+    public double Radio { get; set; }
+    public double Altura { get; set; }
+    public string TipoMaterial { get; set; }
+
+    public Tanque(double radio, double altura, string tipo)
+    {
+        Radio = radio;
+        Altura = altura;
+        TipoMaterial = tipo;
+    }
+
+    // Método para calcular el volumen del tanque
+    public double CalcularVolumen()
+    {
+        return Math.PI * Math.Pow(Radio, 2) * Altura; // Volumen en metros cúbicos
+    }
+}
